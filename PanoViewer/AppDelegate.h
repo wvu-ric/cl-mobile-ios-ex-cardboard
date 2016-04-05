7e224d539e997e87d53424de94c2256f29817ce8
rickykirkendall <rickykirkendall@flocoapps.com>
2016-04-05 13:49:16 -0400

    Initial Commit

---
+++ PanoViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PanoViewer
+//
+//  Created by Ricky Kirkendall on 4/5/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PanoViewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PanoViewer
+//
+//  Created by Ricky Kirkendall on 4/5/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
